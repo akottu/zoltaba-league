e1bdd4662fe2fcfecf2817ea678d74251ea79e91
Anish Kottu <anish.kottu@pendo.io>
2022-07-24 00:13:40 -0400

    changing comp

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,44 @@
-//public class Main {
-//    public static void main(String[] args) {
-//        System.out.println("Hello world!");
-//    }
-//}
-
 import java.util.ArrayList;
-        import java.util.HashMap;
-        import java.util.List;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
 
+        List<Player> players = new ArrayList<>();
+
+        addPlayers(players);
+
+
         runArchive();
 
+    }
+
+    private static void addPlayers(List<Player> players) {
+        players.add(new Player("Anish Kottu", 1));
+        players.add(new Player("Alex Frumkin", 1));
+        players.add(new Player("Mihir Dutta", 1));
+        players.add(new Player("Jeremy Cohen", 1));
+        players.add(new Player("Dylan Zumar", 1));
+        players.add(new Player("Toma Shigaki-Than", 1));
+        players.add(new Player("Hunter Kaufman", 1));
+        players.add(new Player("Sam Auld", 1));
+        players.add(new Player("Albert Wheeler", 1));
+        players.add(new Player("Michael Zheng", 1));
+        players.add(new Player("Jule Cuasay", 1));
+        players.add(new Player("Sahith Reddy", 1));
+        players.add(new Player("Brody Trujillo", 1));
+        players.add(new Player("Surya Tumbapura", 1));
+        players.add(new Player("Darien Heath", 1));
+        players.add(new Player("Michael Williams", 1));
+        players.add(new Player("Chris Yang", 1));
+        players.add(new Player("TJ Sadler", 1));
+        players.add(new Player("Jacob Schuknecht", 1));
+        players.add(new Player("Kody Olthof", 1));
+        players.add(new Player("Alex Halsey", 1));
+        players.add(new Player("Jake Clorfene", 1));
+        players.add(new Player("Player 23", 1));
+        players.add(new Player("Player 24", 1));
     }
 
     public static void runArchive() throws InterruptedException {
